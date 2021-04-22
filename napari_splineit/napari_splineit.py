91c8725998d691eb58ddb8b768973152e80c6325
Soham Mandal <smandal1215@gmail.com>
2021-04-22 05:57:32 +0100

    Add function to select object by clicking on control points

--- napari_splineit/napari_splineit.py
+++ napari_splineit/napari_splineit.py
@@ -19,8 +19,8 @@ def my_basis(basis = "cubic"):
 @magic_factory(
     call_button=True,
     viewer={'visible': False, 'label': ' '},
-    user_input={'mode': 'r', 'label': 'Input Control Points'},
-    output={'mode': 'w', 'label': 'Output Control Points'},
+    user_input={'mode': 'r', 'label': 'Input'},
+    output={'mode': 'w', 'label': 'Output'},
         )
 def napari_splineit(
     viewer : napari.Viewer,
@@ -31,6 +31,8 @@ def napari_splineit(
     
     if user_input.is_file():
         cp = np.load(user_input, allow_pickle = True)
+        #ToDo: Need fix
+        cp = cp.astype('int')
     else:
         cp = np.array([
             [[14,19], [14,35], [33,35], [33,19]],
@@ -39,10 +41,9 @@ def napari_splineit(
         ])
 
     for i in range(len(cp)):
-        viewer.add_points(cp[i], size=2, name='control points ' + str(i))
-        
-    cp = torch.from_numpy(cp).float()
+        viewer.add_points(cp[i], size=3, name='control points ' + str(i))
 
+    cp = torch.from_numpy(cp).float()
     phi_generator(cp.shape[1], 500, 'cubic')
 
     phi = np.load('./phi_' + str(cp.shape[1]) + '.npy')
@@ -52,11 +53,11 @@ def napari_splineit(
     curve = torch.cat([curve, torch.reshape(curve[:,0], (-1,1,2))], axis = 1)
 
     for i in range(len(cp)):
-        viewer.add_shapes(curve[i], shape_type='path', edge_width=0.5,
+        viewer.add_shapes(curve[i], shape_type='path', edge_width=3,
                           edge_color='coral', face_color='royalblue', name='spline ' + str(i))
         
     for i in range(len(cp)):
-        cp_polygon = viewer.add_shapes(cp[i], shape_type='polygon', edge_width=0.2,
+        cp_polygon = viewer.add_shapes(cp[i], shape_type='polygon', edge_width=2,
                                        edge_color='teal', face_color='transparent', name='control polygon ' + str(i))
     
     objects_count = cp.shape[0]
@@ -93,20 +94,20 @@ def cp_addition(cp_current):
 def update_layers(updates):
     viewer = updates[0]    
     idx = updates[1]
-    gui_basis = updates[2]
+    my_basis = updates[2]
     
     idx_cp = 'control points ' + str(idx)
     idx_spline = 'spline ' + str(idx)
     idx_control_polygon = 'control polygon ' + str(idx)
     
-    if (gui_basis == 'linear'):
+    if (my_basis == 'linear'):
         basis = sg.B1()
-    elif (gui_basis == 'cubic'):
+    elif (my_basis == 'cubic'):
         basis = sg.B3()
     
     cp = viewer.layers[idx_cp].data    
     cp = torch.from_numpy(cp).float()
-    phi_generator(len(cp), 500, gui_basis)
+    phi_generator(len(cp), 500, my_basis)
     phi = np.load('./phi_' + str(len(cp)) + '.npy')
     phi = torch.from_numpy(phi)
     SplineContour = sg.SplineCurveSample(len(cp),basis,True,cp)
@@ -131,16 +132,25 @@ def update_layers(updates):
         shape_type='polygon',
     ) 
     
+    viewer.active_layer = viewer.layers[idx_cp] 
+    
 @thread_worker(connect={'yielded': update_layers})
 def get_cp(viewer, my_basis, objects_count, output):
     cp_old = []
     m_old = []
+    
+    #ToDo: generalize object count 
+    objects_count = (len(viewer.layers))//3
+    
     for i in range(objects_count):
         idx_cp = 'control points ' + str(i)
         cp_old.append(viewer.layers[idx_cp].data.copy())
         m_old.append(viewer.layers[idx_cp].data.shape[0]) 
     basis_old = my_basis.basis.value
-    while True:
+    
+    while True:             
+        objects_count = (len(viewer.layers))//3
+        
         basis_current = my_basis.basis.value
         if basis_old == basis_current:
             pass
@@ -152,17 +162,25 @@ def get_cp(viewer, my_basis, objects_count, output):
         for i in range(objects_count):
             idx_object = 'control points ' + str(i)
             m_current.append(viewer.layers[idx_object].data.shape[0])
+        
+        if len(m_current) > len(m_old):
+            m_old.append(m_old[0])
+        
         idx_m_update = list(map(np.less,m_old,m_current))
         idx_m_update = np.where(np.array(idx_m_update) == 1)[0]
         if len(idx_m_update) != 0:
             viewer.layers['control points ' + str(idx_m_update[0])].data = cp_addition(viewer.layers
                                                                            ['control points ' + str(idx_m_update[0])].data) 
             m_old = m_current.copy() 
-        
+        #because len of cp current is more, so equal not working
         cp_current = []
         for i in range(objects_count):
             idx_object = 'control points ' + str(i)
-            cp_current.append(viewer.layers[idx_object].data.copy())     
+            cp_current.append(viewer.layers[idx_object].data.copy())
+        
+        if len(cp_current) > len(cp_old):
+            cp_old.append(cp_old[0])
+        
         idx_cp_update = list(map(np.array_equal,cp_old,cp_current))
         idx_cp_update = np.where(np.array(idx_cp_update) == 0)[0]
         if len(idx_cp_update) != 0: 
@@ -172,4 +190,29 @@ def get_cp(viewer, my_basis, objects_count, output):
         time.sleep(0.1)
         
         if output is not None:
-            np.save( output, np.array(cp_current, dtype = object))
+            np.save(output, np.array(cp_current, dtype = object))
+            
+        @viewer.mouse_drag_callbacks.append
+        def track_mouse_click(viewer, event):
+            yield
+            if event.type == 'mouse_release': 
+                mouse_position = np.asarray(event.position)
+                mouse_position = np.round(mouse_position).astype(int)
+                
+                objects_count = (len(viewer.layers))//3
+                idx_active_layer = np.zeros(objects_count).astype('int')
+                for i in range(objects_count):
+                    cp_list = viewer.layers['control points ' + str(i)].data            
+                    for j in range(len(cp_list)):
+                        #ToDo: include a tolerance region of cp_list to compare
+                        if(np.array_equal(mouse_position, cp_list[j])):
+                            idx_active_layer[i] = 1
+                            break                      
+                    if np.count_nonzero(idx_active_layer) > 0:
+                        break
+
+                idx_active_layer = np.where(idx_active_layer)[0]
+
+                if (len(idx_active_layer)) > 0:
+                    idx_active_layer = idx_active_layer[0]
+                    viewer.active_layer = viewer.layers['control points ' + str(idx_active_layer)]
