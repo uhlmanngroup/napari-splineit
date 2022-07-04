7351f4141b8ad24bd674f722c6adb8dfbfcede1e
Soham Mandal <smandal1215@gmail.com>
2022-07-04 10:07:14 +0100

    Update UhlmannSplines to expect knots as inputs instead of control points

--- src/napari_splineit/interpolation/uhlmann.py
+++ src/napari_splineit/interpolation/uhlmann.py
@@ -6,15 +6,15 @@ from .interpolator_base import InterpolatorBase
 from qtpy.QtWidgets import QWidget
 
 
-def curve_from_cp(cp):
+def curve_from_knots(knots):
 
-    if cp.shape[0] > 3:
-        cp = getCoefsFromKnots(cp, "cubic")
-        phi = phi_generator_impl(cp.shape[0], 20 * cp.shape[0], "cubic")
-        SplineContour = SplineCurveSample(cp.shape[0], B3(), True, cp)
+    if knots.shape[0] > 3:
+        ctrl_points = getCoefsFromKnots(knots, "cubic")
+        phi = phi_generator_impl(ctrl_points.shape[0], 20 * ctrl_points.shape[0], "cubic")
+        SplineContour = SplineCurveSample(ctrl_points.shape[0], B3(), True, ctrl_points)
         curve = SplineContour.sample(phi)
         return curve
-    return cp.copy()
+    return knots.copy()
 
 
 class UhlmannSplinesUI(QWidget):
@@ -28,5 +28,5 @@ class UhlmannSplines(InterpolatorBase):
     UI = UhlmannSplinesUI
     name = "UhlmannSplines"
 
-    def __call__(self, ctrl_points):
-        return curve_from_cp(ctrl_points)
+    def __call__(self, knots):
+        return curve_from_knots(knots)
